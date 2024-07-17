f76aa7dc0a25acc6c58e2059a235735bfb4c645d
AqilAqayev404notfounded <aqilaqayev777@gmail.com>
2024-07-17 17:08:00 +0400

    Add project files.

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+namespace CustomList;
+
+public class CustomList<T> : IEnumerable<T>
+{
+    public T[] ListCustom;
+    public T this[int index]
+    {
+        get { return ListCustom[index]; }
+        set { ListCustom[index] = value; }
+    }
+    public CustomList()
+    {
+        ListCustom = new T[0];
+    }
+    public void Add(T item)
+    {
+        Array.Resize(ref ListCustom, ListCustom.Length + 1);
+        ListCustom[ListCustom.Length - 1] = item;
+    }
+    public T Find(Predicate<T> predicate)
+    {
+        if (predicate == null)
+        {
+            throw new ArgumentNullException();
+        }
+        for (int i = 0; i < ListCustom.Length; i++)
+        {
+            if (predicate(ListCustom[i]))
+            {
+                return ListCustom[i];
+            }
+        }
+
+        return default(T);
+
+    }
+    public void Remove(T item)
+    {
+        for (int i = 0;i< ListCustom.Length; i++)
+        {
+            if (ListCustom[i].Equals(item))
+            {
+                for (int j = i; j < ListCustom.Length - 1; j++)
+                {
+                    ListCustom[j] = ListCustom[j + 1];
+                }
+                Array.Resize(ref ListCustom, ListCustom.Length - 1);
+                
+            }
+
+        }
+    }
+    public void RemoveAll()
+    {
+        Array.Resize(ref ListCustom, 0);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+
+        foreach (T item in ListCustom)
+        {
+            yield return item;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        foreach (T item in ListCustom)
+        {
+            yield return item;
+        }
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,29 @@
+﻿using CustomList;
+
+CustomList<int> customList = new CustomList<int>();
+customList.Add(1);
+customList.Add(2);
+customList.Add(3);
+customList.Add(4);
+customList.Add(5);
+customList.Remove(4);
+foreach (int x in customList)
+{
+    Console.WriteLine(x);
+}
+Console.WriteLine("===========================");
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+
+list.Add(4);
+list.Add(5);
+foreach (int x in list)
+{
+    Console.WriteLine(x);
+}
+foreach (var item in list.FindAll(x => x == 5))
+{
+    Console.WriteLine(item);
+}
